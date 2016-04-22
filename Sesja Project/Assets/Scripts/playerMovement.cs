4abfa97bdff8725b677ba5aa3157a32fb4ba4480
adrianwoj <adrian_woj@o2.pl>
2016-04-22 21:35:01 +0200

    Classroom basic

---
+++ Sesja Project/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+    public int speed = 5;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed;
+
+    }
+}
