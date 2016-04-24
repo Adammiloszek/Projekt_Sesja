1b59691021d015358544571e5fe6f67806852a22
bkoziel <blaktross@gmail.com>
2016-04-24 20:14:37 +0200

    fov+sciaganie

---
+++ Sesja Project/Assets/Scripts/FovController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FovController : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Sesja Project/Assets/Scripts/playerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour {
+
+    public Slider slider;
+    private bool studentInRange;
+    private float currentPoints;
+
+    // Use this for initialization
+    void Start () {
+        studentInRange = false;
+        currentPoints = 0.0f;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Student")
+        {
+            studentInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Student")
+        {
+
+            studentInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (studentInRange == true) currentPoints += 0.02f;
+        slider.value = currentPoints;
+    }
+}
