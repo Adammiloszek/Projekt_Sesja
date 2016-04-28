0df44e0c7392a4d729eedf9ea76f3fab059b4d71
kamil <Kamil_wit1@wp.pl>
2016-04-28 22:08:13 +0200

    Fix buga dotyczącego ruchu, dodanie colliderów i poprawa ich.

--- Sesja Project/Assets/PacmanMove.cs
+++ Sesja Project/Assets/PacmanMove.cs
@@ -17,18 +17,15 @@ public class PacmanMove : MonoBehaviour {
         Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
         GetComponent<Rigidbody2D>().MovePosition(p);
 
-        // Check for Input if not moving
-        if ((Vector2)transform.position == dest)
-        {
-            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+
+            if (Input.GetKey(KeyCode.UpArrow) )
                 dest = (Vector2)transform.position + Vector2.up;
-            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+            if (Input.GetKey(KeyCode.RightArrow) )
                 dest = (Vector2)transform.position + Vector2.right;
-            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+            if (Input.GetKey(KeyCode.DownArrow) )
                 dest = (Vector2)transform.position - Vector2.up;
-            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+            if (Input.GetKey(KeyCode.LeftArrow) )
                 dest = (Vector2)transform.position - Vector2.right;
-        }
 
         // Animation Parameters
         Vector2 dir = dest - (Vector2)transform.position;
@@ -36,11 +33,5 @@ public class PacmanMove : MonoBehaviour {
         GetComponent<Animator>().SetFloat("DirY", dir.y);
     }
 
-    bool valid(Vector2 dir)
-    {
-        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
-        Vector2 pos = transform.position;
-        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
-        return (hit.collider == GetComponent<Collider2D>());
-    }
+
 }
