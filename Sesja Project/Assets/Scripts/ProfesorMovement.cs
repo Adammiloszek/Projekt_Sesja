8f3f03a4992d0af5f9283afbe74e85a11c973bd4
adrianwoj <adrian_woj@o2.pl>
2016-05-17 00:02:23 +0200

    Reorganizacja plików projektu, poruszanie profesora i drobne fixy

--- Sesja Project/Assets/Scripts/ProfesorMovement.cs
+++ Sesja Project/Assets/Scripts/ProfesorMovement.cs
@@ -15,11 +15,17 @@ public class ProfesorMovement : MonoBehaviour {
     Transform point;
     bool onPoint=false;
     bool readyToMove = true;
+    bool rotate=false;
     int waiting=0;
+    int waiting2 = 0;
     float step;
     List<Transform> listPoints= new List<Transform>();
     int direction;
-    
+    int angle;
+
+
+
+
     void Start()
     {
         step = speed * Time.deltaTime;
@@ -31,8 +37,10 @@ public class ProfesorMovement : MonoBehaviour {
     {
         if (point.position == Profesor.position)
         {
+
             Profesor.position = point.position;
             onPoint = true;
+            angle = direction * 90;
             NewPoint();
             readyToMove = false;
 
@@ -44,25 +52,34 @@ public class ProfesorMovement : MonoBehaviour {
         
         if (onPoint)
         {
-
-            if (waiting <= 360/2)
+            if (angle + 2 * waiting <= 359)
             {
                 waiting++;
-                Profesor.localRotation = Quaternion.Euler(new Vector3(2*waiting, 90, 90));
+                Debug.Log(waiting);
+                Profesor.localRotation = Quaternion.Euler(new Vector3(angle + 2 * waiting, 90, 90));
             }
+            else rotate = true;
 
-            if (waiting > 360 / 2  && waiting-180 < direction*90)
+            //if (waiting <= 360/2)
+            //{
+            //    waiting++;
+            //    Profesor.localRotation = Quaternion.Euler(new Vector3(2*waiting, 90, 90));
+            //}
+
+            if (rotate && waiting2 < direction * 90 / 2)
             {
-                waiting++;
-                Profesor.localRotation = Quaternion.Euler(new Vector3(2 * waiting, 90, 90));
+                
+                waiting2++;
+                Profesor.localRotation = Quaternion.Euler(new Vector3(2 * waiting2, 90, 90));
             }
-
         }
         else
         {
             if (readyToMove)
             {
+                rotate = false;
                 waiting = 0;
+                waiting2 = 0;
                 Profesor.position = Vector3.MoveTowards(Profesor.position, point.position, step);
                 
             }
@@ -77,28 +94,27 @@ public class ProfesorMovement : MonoBehaviour {
 
     IEnumerator WaitCoroutine()
     {
-        yield return new WaitForSeconds(4.0f);
+        yield return new WaitForSeconds(5f);
         readyToMove = true;
         onPoint = false;
         RotateToward();
     }
 
-
     void RotateToward()
     {
-
 
 
         Debug.Log(Profesor.position +"  "+ point.position );
 
         if (Profesor.position.x > point.position.x ) { Profesor.localRotation = Quaternion.Euler(new Vector3(180, 90, 90)); direction = 2; }
         if (Profesor.position.x < point.position.x ) { Profesor.localRotation = Quaternion.Euler(new Vector3(0, 90, 90)); direction = 0; }
-        if (Profesor.position.y < point.position.y) { Profesor.localRotation = Quaternion.Euler(new Vector3(-90, 90, 90)); direction = 3; }
-            if (Profesor.position.y > point.position.y) { Profesor.localRotation = Quaternion.Euler(new Vector3(90, 90, 90)); direction = 1; }
+        if (Profesor.position.y < point.position.y) { Profesor.localRotation = Quaternion.Euler(new Vector3(270, 90, 90)); direction = 3; }
+        if (Profesor.position.y > point.position.y) { Profesor.localRotation = Quaternion.Euler(new Vector3(90, 90, 90)); direction = 1; }
 
         //_direction = (point.position - transform.position).normalized;
         //_lookRotation = Quaternion.LookRotation(_direction);
         //transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 1000);
+
     }
 
     void NewPoint()
@@ -112,7 +128,6 @@ public class ProfesorMovement : MonoBehaviour {
             {
                 listPoints.Add(item);
             }
-
         }
         point = listPoints[Random.Range(0, listPoints.Count)];
         
