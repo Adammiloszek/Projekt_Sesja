492180d79bdcf875997835ea8a122b6a3ea9174d
kamil <Kamil_wit1@wp.pl>
2016-06-01 23:06:06 +0200

    podział grafik duszków na 8 części

--- Sesja Project/Assets/Pacman/scripts/GhostMove.cs
+++ Sesja Project/Assets/Pacman/scripts/GhostMove.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -33,39 +33,3 @@ public class GhostMove : MonoBehaviour
     }
 }
 
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class GhostMove : MonoBehaviour
-{
-    public Transform[] waypoints;
-    int cur = 0;    
-
-    public float speed = 0.3f;
-
-    void FixedUpdate()
-    {
-        if  ((transform.position.x - waypoints[cur].position.x > 0.01 || transform.position.x - waypoints[cur].position.x <-0.01) ||
-            (transform.position.y - waypoints[cur].position.y > 0.01 || transform.position.y - waypoints[cur].position.y < -0.01))
-        
-        {
-            Vector2 p = Vector2.MoveTowards(transform.position,
-                                            waypoints[cur].position,
-                                            speed);
-            GetComponent<Rigidbody2D>().MovePosition(p);
-        }
-        else cur = (cur + 1) % waypoints.Length;
-        // Animation
-        Vector2 dir = waypoints[cur].position - transform.position;
-        GetComponent<Animator>().SetFloat("DirX", dir.x);
-        GetComponent<Animator>().SetFloat("DirY", dir.y);
-    }
-    void OnTriggerEnter2D(Collider2D co)
-    {
-        if (co.name == "pacman")
-            Destroy(co.gameObject);
-    }
-}
-
->>>>>>> 8e64fcdd9aa788b7d561c3c84d7aa705fa0c25da
