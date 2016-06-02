15a1bde89d91cb8ba168ec9c40663e05c574233a
kamil <Kamil_wit1@wp.pl>
2016-06-02 14:45:18 +0200

    Duszki generowane z poziomu kodu, ich ilosc zalezy od statystyki wiedza

--- Sesja Project/Assets/Pacman/scripts/GhostMovePathInCode.cs
+++ Sesja Project/Assets/Pacman/scripts/GhostMovePathInCode.cs
@@ -3,24 +3,77 @@ using System.Collections;
 
 public class GhostMovePathInCode : MonoBehaviour {
 
-	private Transform[] waypoints = new Transform[9];
+	private Transform[] waypoints;
 	
 	public Rigidbody2D rb;
+
+	public Animator animator;
 	
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D> ();
-		waypoints[0] = GameObject.Find("Waypoint50").transform;
-		waypoints[1] = GameObject.Find("Waypoint52").transform;
-		waypoints[2] = GameObject.Find("Waypoint91").transform;
-		waypoints[3] = GameObject.Find("Waypoint92").transform;
-		waypoints[4] = GameObject.Find("Waypoint72").transform;
-		waypoints[5] = GameObject.Find("Waypoint78").transform;
-		waypoints[6] = GameObject.Find("Waypoint27").transform;
-		waypoints[7] = GameObject.Find("Waypoint21").transform;
-		waypoints[8] = GameObject.Find("Waypoint42").transform;
+
+		if (this.gameObject.name == "inky(Clone)") {
+			waypoints = new Transform[9];
+			Debug.Log("blinky ");
+			waypoints [0] = GameObject.Find ("Waypoint50").transform;
+			waypoints [1] = GameObject.Find ("Waypoint52").transform;
+			waypoints [2] = GameObject.Find ("Waypoint91").transform;
+			waypoints [3] = GameObject.Find ("Waypoint92").transform;
+			waypoints [4] = GameObject.Find ("Waypoint72").transform;
+			waypoints [5] = GameObject.Find ("Waypoint78").transform;
+			waypoints [6] = GameObject.Find ("Waypoint27").transform;
+			waypoints [7] = GameObject.Find ("Waypoint21").transform;
+			waypoints [8] = GameObject.Find ("Waypoint42").transform;
+		}
+
+
+		if (this.gameObject.name == "blinky(Clone)") {
+			waypoints = new Transform[7];
+			Debug.Log("blinky ");
+			waypoints [0] = GameObject.Find ("Waypoint44").transform;
+			waypoints [1] = GameObject.Find ("Waypoint23").transform;
+			waypoints [2] = GameObject.Find ("Waypoint26").transform;
+			waypoints [3] = GameObject.Find ("Waypoint46").transform;
+			waypoints [4] = GameObject.Find ("Waypoint45").transform;
+			waypoints [5] = GameObject.Find ("Waypoint63").transform;
+			waypoints [6] = GameObject.Find ("Waypoint62").transform;
+
+		}
+
+
+		if (this.gameObject.name == "clyde(Clone)") {
+			waypoints = new Transform[5];
+			Debug.Log("clyde ");
+			waypoints [0] = GameObject.Find ("Waypoint09").transform;
+			waypoints [1] = GameObject.Find ("Waypoint99").transform;
+			waypoints [2] = GameObject.Find ("Waypoint98").transform;
+			waypoints [3] = GameObject.Find ("Waypoint77").transform;	
+			waypoints [4] = GameObject.Find ("Waypoint78").transform;
+
+			
+		}
+
 		
-		
+		if (this.gameObject.name == "pinky(Clone)") {
+			waypoints = new Transform[12];
+			Debug.Log("pinky ");
+			waypoints [0] = GameObject.Find ("Waypoint40").transform;
+			waypoints [1] = GameObject.Find ("Waypoint41").transform;
+			waypoints [2] = GameObject.Find ("Waypoint51").transform;
+			waypoints [3] = GameObject.Find ("Waypoint52").transform;	
+			waypoints [4] = GameObject.Find ("Waypoint91").transform;
+			waypoints [5] = GameObject.Find ("Waypoint92").transform;
+			waypoints [6] = GameObject.Find ("Waypoint72").transform;
+			waypoints [7] = GameObject.Find ("Waypoint73").transform;	
+			waypoints [8] = GameObject.Find ("Waypoint23").transform;
+			waypoints [9] = GameObject.Find ("Waypoint22").transform;
+			waypoints [10] = GameObject.Find ("Waypoint02").transform;
+			waypoints [11] = GameObject.Find ("Waypoint00").transform;
+
+			
+			
+		}
 		
 	}
 	
@@ -47,13 +100,13 @@ public class GhostMovePathInCode : MonoBehaviour {
 		}
 		else index = (index + 1) % waypoints.Length;
 		// Animation
-		Vector2 dir = waypoints[index].position - transform.position;
-		GetComponent<Animator>().SetFloat("DirX", dir.x);
-		GetComponent<Animator>().SetFloat("DirY", dir.y);
+	/*	Vector2 dir = waypoints[index].position - transform.position;
+		animator.SetFloat("DirX", dir.x);
+		animator.SetFloat("DirY", dir.y);	*/ // po zrobieniu animacji
 	}
 	void OnTriggerEnter2D(Collider2D co)
 	{
-		if (co.name == "pacman")
+		if (co.tag == "PacMan")
 			Destroy(co.gameObject);
 	}
 }
---
+++ Sesja Project/Assets/createGhosts.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createGhosts : MonoBehaviour {
+
+	public GameObject pinky;
+	public GameObject clyde;
+	public GameObject inky;
+	public GameObject blinky;
+	public int knowledge; // w zaleznosci od wiedzy od 0 do 4, 0 wiedzy ->  4 duszki, 4 wiedzy -> 0 duszków
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject[] ghosts = new GameObject[4];
+		ghosts [0] = pinky;
+		ghosts [1] = clyde;
+		ghosts [2] = inky;
+		ghosts [3] = blinky;
+
+		Vector2[] startingVectors = new Vector2[4];
+		startingVectors [0] = new Vector2 ((-13f + 15f), (10f + 20f));
+		startingVectors [1] = new Vector2 (12.00f + 15.0f, -18.00f + 20.0f);
+		startingVectors [2] = new Vector2 (26.82f, 29.94f);
+		startingVectors [3] = new Vector2 (15f, 17f);
+
+
+		/*
+		GameObject pinky = GameObject.Find ("pinky");
+		GameObject clyde = GameObject.Find ("clyde");
+		GameObject inky = GameObject.Find ("inky");
+		GameObject blinky = GameObject.Find ("blinky"); */
+		
+	/*	Instantiate (pinky, new Vector2 ((-13f+15f), (10f+20f)), Quaternion.identity);
+		Instantiate (clyde, new Vector2(12.00f+15.0f, -18.00f+20.0f), Quaternion.identity);
+		Instantiate (inky, new Vector2 (26.82f, 29.94f), Quaternion.identity);
+		Instantiate (blinky, new Vector2 (15f, 17f), Quaternion.identity);	*/
+
+		for (int i = 1; i <= ghosts.Length - knowledge; i++) {
+
+			Instantiate(ghosts[i-1], startingVectors[i-1], Quaternion.identity);
+
+	
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
