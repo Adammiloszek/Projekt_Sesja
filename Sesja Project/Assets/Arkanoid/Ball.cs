02c8ff73bec850a43f8ab03ddf076d75f10a84be
Karol Kostrzewa <Karol Kostrzewa>
2016-06-05 19:12:26 +0200

    nowe grafiki, porażka

--- Sesja Project/Assets/Arkanoid/Ball.cs
+++ Sesja Project/Assets/Arkanoid/Ball.cs
@@ -5,9 +5,24 @@ public class Ball : MonoBehaviour {
 
     float speed = 100;
 
+ 
+
 	// Use this for initialization
 	void Start () {
         GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GetComponent<Rigidbody2D>().position.y < -105)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * 0;
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * 0;
+           
+        }
     }
 
     //w jaka czesc podstawki uderzono
--- Sesja Project/Assets/Arkanoid/Racket.cs
+++ Sesja Project/Assets/Arkanoid/Racket.cs
@@ -5,20 +5,28 @@ public class Racket : MonoBehaviour {
 
     public float speed = 150;
 
-    void FixedUpdate()
-    {
-        float h = Input.GetAxisRaw("Horizontal");
+    GameObject ballObj;
 
-       
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
-    }
     // Use this for initialization
     void Start () {
-	
-	}
+	ballObj = GameObject.Find("ball");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float h = Input.GetAxisRaw("Horizontal");
+        
+
+        if(ballObj.GetComponent<Rigidbody2D>().velocity.x == 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * h * 0;
+
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+        }
+    }
+
+   
 }
