a1efbdf1e24347de03c13c15370aaa25895d8cb1
kamil <Kamil_wit1@wp.pl>
2016-06-06 00:24:53 +0200

    przykładowe Menu

---
+++ Sesja Project/Assets/Menu/scripts/QuitGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour {
+
+	public Button exitBtn;
+
+	void Start () {
+
+		exitBtn.onClick.AddListener (() => {
+			Application.Quit ();
+		});
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
