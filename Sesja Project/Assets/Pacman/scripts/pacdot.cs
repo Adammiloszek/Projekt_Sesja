9e834a6e61aeed110b88be12ce658d233022b13c
strza1ka <aleks.ciok@gmail.com>
2016-06-14 20:18:22 +0200

    Sceny przejściowe, oceny, punkty

--- Sesja Project/Assets/Pacman/scripts/pacdot.cs
+++ Sesja Project/Assets/Pacman/scripts/pacdot.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class pacdot : MonoBehaviour {
-
+public static int punkty = 1550;
 	void OnTriggerEnter2D(Collider2D co)
     {
 
@@ -24,7 +24,6 @@ public class pacdot : MonoBehaviour {
         if (collisionInfo.gameObject.name == "maze")
         {
             Destroy(collisionInfo.gameObject);
-
         }
     }
 
