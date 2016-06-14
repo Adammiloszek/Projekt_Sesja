80f479a907d1b3607d8cd65a1a3dd54fa0e1fa4e
strza1ka <aleks.ciok@gmail.com>
2016-06-14 20:20:35 +0200

    Wrzucam scenki

---
+++ Sesja Project/Assets/Pacman/scripts/StartPacman.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartPacman : MonoBehaviour
+{
+
+    string scena;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        scena = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("return"))
+        {
+            if (scena == "startowa" || scena == "powtorka")
+                SceneManager.LoadScene("pacman");
+            else if (scena == "pacman")
+                SceneManager.LoadScene("umrzyj");
+            else if (scena == "umrzyj")
+            {
+                LastScene.myLastScene = Application.loadedLevelName;
+                Application.LoadLevel("Credits");
+            }
+        }
+    }
+}
